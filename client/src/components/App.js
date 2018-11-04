b564d839461feaf92c5040097ce4313d4411a8a7
jenjensha <14373089+jenjensha@users.noreply.github.com>
2018-11-04 19:15:29 +0100

    Add image and change font color

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,20 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Timeline from './Timeline';
 
-
-
 class App extends React.Component {
-
   render() {
     return (
-<Router>
-  <Switch>
-  <Route exact path="/" component={Home}/>
-  <Route exact path="/feed" component={Timeline}/>
-  </Switch>
-  </Router>
-
-    )
+      <div className="reddit-background">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/feed" component={Timeline} />
+          </Switch>
+        </Router>
+      </div>
+    );
   }
 }
 
